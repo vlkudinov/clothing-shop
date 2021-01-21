00a3fe93ce9dd5e81dce21d0f79157135f1f9577
Vladimir Kudinov <vlkudinov@gmail.com>
2021-01-21 09:33:46 +0300

    added header component with menu links

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import './App.scss'
 
-import Homepage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-
-
-
+import Homepage from 'pages/homepage/homepage.component';
+import ShopPage from 'pages/shop/shop.component';
+import Header from 'components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route path='/shop' component={ShopPage}/>
